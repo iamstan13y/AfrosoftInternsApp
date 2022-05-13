4e4f38ffeb4fac4def8a377e7b1f70f0eb47931e
iamstan13y <iamstan13y@gmail.com>
2022-05-13 14:52:39 +0200

    Clean up II

--- AfrosoftInterns/Program.cs
+++ AfrosoftInterns/Program.cs
@@ -1,12 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Security.Authentication;
-using System.Threading.Tasks;
 
 namespace AfrosoftInterns
 {
--- AfrosoftInterns/Startup.cs
+++ AfrosoftInterns/Startup.cs
@@ -5,10 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AfrosoftInterns
 {
